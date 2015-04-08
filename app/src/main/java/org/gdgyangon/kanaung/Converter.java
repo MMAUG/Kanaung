0334fb8ee9ee8aa3a613c5405d7253edf4759d9f
Ye Myat Thu <thu.yemyat@gmail.com>
2015-04-09 00:29:09 +0630

    Some tweak on zgToUni regex

--- app/src/main/java/org/gdgyangon/kanaung/Converter.java
+++ app/src/main/java/org/gdgyangon/kanaung/Converter.java
@@ -1,5 +1,8 @@
 package org.gdgyangon.kanaung;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * Created by trhura on 10/22/14.
  */
@@ -24,6 +27,7 @@ public class Converter {
   // You should have received a copy of the GNU General Public License
   // along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
 
+
   public static String zg12uni51(String input_text) {
     String output_text = input_text;
     output_text = output_text.replaceAll("\\u106a", "\u1009");
@@ -35,22 +39,17 @@ public class Converter {
     output_text = output_text.replaceAll("\\u108f", "\u1014");
     output_text = output_text.replaceAll("\\u1012", "\u1012");
     output_text = output_text.replaceAll("\\u1013", "\u1013");
-    output_text = output_text.replaceAll("[\\u103d\\u1087]", "\u103d");
+    output_text = output_text.replaceAll("[\\u103d\\u1087]", "\u103e");
     output_text = output_text.replaceAll("\\u103c", "\u103d");
-    output_text = output_text.replaceAll("[\\u103b\\u107e\\u107f\\u1080\\u1081\\u1082\\u1083\\u1084]",
-        "\u103c");
+    output_text = output_text.replaceAll("[\\u103b\\u107e\\u107f\\u1080\\u1081\\u1082\\u1083\\u1084]", "\u103c");
     output_text = output_text.replaceAll("[\\u103a\\u107d]", "\u103b");
     output_text = output_text.replaceAll("\\u103d\\u103b", "\u103b\u103d");
     output_text = output_text.replaceAll("\\u108a", "\u103d\u103d");
     output_text = output_text.replaceAll("\\u103d\\u103d", "\u103d\u103d");
-    output_text = output_text.replaceAll("((?:\\u1031)?)((?:\\u103c)?)([\\u1000-\\u1021])\\u1064",
-        "\u1064$1$2$3");
-    output_text = output_text.replaceAll("((?:\\u1031)?)((?:\\u103c)?)([\\u1000-\\u1021])\\u108b",
-        "\u1064$1$2$3\u102d");
-    output_text = output_text.replaceAll("((?:\\u1031)?)((?:\\u103c)?)([\\u1000-\\u1021])\\u108c",
-        "\u1064$1$2$3\u102e");
-    output_text = output_text.replaceAll("((?:\\u1031)?)((?:\\u103c)?)([\\u1000-\\u1021])\\u108d",
-        "\u1064$1$2$3\u1036");
+    output_text = output_text.replaceAll("((?:\\u1031)?)((?:\\u103c)?)([\\u1000-\\u1021])\\u1064", "\u1064$1$2$3");
+    output_text = output_text.replaceAll("((?:\\u1031)?)((?:\\u103c)?)([\\u1000-\\u1021])\\u108b", "\u1064$1$2$3\u102d");
+    output_text = output_text.replaceAll("((?:\\u1031)?)((?:\\u103c)?)([\\u1000-\\u1021])\\u108c", "\u1064$1$2$3\u102e");
+    output_text = output_text.replaceAll("((?:\\u1031)?)((?:\\u103c)?)([\\u1000-\\u1021])\\u108d", "\u1064$1$2$3\u1036");
     output_text = output_text.replaceAll("\\u105a", "\u102b\u103a");
     output_text = output_text.replaceAll("\\u108e", "\u102d\u1036");
     output_text = output_text.replaceAll("\\u1033", "\u102f");
@@ -59,9 +58,7 @@ public class Converter {
     output_text = output_text.replaceAll("\\u1089", "\u103d\u1030");
     output_text = output_text.replaceAll("\\u1039", "\u103a");
     output_text = output_text.replaceAll("[\\u1094\\u1095]", "\u1037");
-    output_text = output_text.replaceAll(
-        "([\\u1000-\\u1021])([\\u102c\\u102d\\u102e\\u1032\\u1036]){1,2}([\\u1060\\u1061\\u1062\\u1063\\u1065\\u1066\\u1067\\u1068\\u1069\\u1070\\u1071\\u1072\\u1073\\u1074\\u1075\\u1076\\u1077\\u1078\\u1079\\u107a\\u107b\\u107c\\u1085])",
-        "$1$3$2");
+    output_text = output_text.replaceAll("([\\u1000-\\u1021])([\\u102c\\u102d\\u102e\\u1032\\u1036]){1,2}([\\u1060\\u1061\\u1062\\u1063\\u1065\\u1066\\u1067\\u1068\\u1069\\u1070\\u1071\\u1072\\u1073\\u1074\\u1075\\u1076\\u1077\\u1078\\u1079\\u107a\\u107b\\u107c\\u1085])", "$1$3$2");
     output_text = output_text.replaceAll("\\u1064", "\u1004\u103a\u1039");
     output_text = output_text.replaceAll("\\u104e", "\u104e\u1004\u103a\u1038");
     output_text = output_text.replaceAll("\\u1086", "\u103f");
@@ -92,20 +89,15 @@ public class Converter {
     output_text = output_text.replaceAll("\\u1097", "\u100b\u1039\u100b");
     output_text = output_text.replaceAll("\\u106f", "\u100e\u1039\u100d");
     output_text = output_text.replaceAll("\\u106e", "\u100d\u1039\u100d");
-    output_text = output_text.replaceAll("(\\u103c)([\\u1000-\\u1021])((?:\\u1039[\\u1000-\\u1021])?)",
-        "$2$3$1");
+    output_text = output_text.replaceAll("(\\u103c)([\\u1000-\\u1021])((?:\\u1039[\\u1000-\\u1021])?)", "$2$3$1");
     output_text = output_text.replaceAll("(\\u103d)(\\u103d)([\\u103b\\u103c])", "$3$2$1");
     output_text = output_text.replaceAll("(\\u103d)([\\u103b\\u103c])", "$2$1");
     output_text = output_text.replaceAll("(\\u103d)([\\u103b\\u103c])", "$2$1");
-    //output_text = output_text.replaceAll("(?<=([\\u1000-\\u101c\\u101e-\\u102a\\u102c\\u102e-\\u103d\\u104c-\\u109f]))(\\u1040)(?=\\s)?", "\u101d");
-    //output_text = output_text.replaceAll("(?<=(\\u101d))(\\u1040)(?=\\s)?", "\u101d");
-    //output_text = output_text.replaceAll("(?<=([\\u1000-\\u101c\\u101e-\\u102a\\u102c\\u102e-\\u103d\\u104c-\\u109f\\s]))(\\u1047)", "\u101b");
-    output_text = output_text.replaceAll(
-        "(\\u1047)(?=[\\u1000-\\u101c\\u101e-\\u102a\\u102c\\u102e-\\u103d\\u104c-\\u109f\\s])",
-        "\u101b");
-    output_text = output_text.replaceAll(
-        "((?:\\u1031)?)([\\u1000-\\u1021])((?:\\u1039[\\u1000-\\u1021])?)((?:[\\u102d\\u102e\\u1032])?)([\\u1036\\u1037\\u1038]{0,2})([\\u103b-\\u103d]{0,3})((?:[\\u102f\\u1030])?)([\\u1036\\u1037\\u1038]{0,2})((?:[\\u102d\\u102e\\u1032])?)",
-        "$2$3$6$1$4$9$7$5$8");
+    output_text = output_text.replaceAll("(?<=([\\u1000-\\u101c\\u101e-\\u102a\\u102c\\u102e-\\u103d\\u104c-\\u109f]))(\\u1040)\\s", "\u101d");
+    output_text = output_text.replaceAll("(?<=(\\u101d))(\\u1040)\\s", "\u101d");
+    output_text = output_text.replaceAll("(?<=([\\u1000-\\u101c\\u101e-\\u102a\\u102c\\u102e-\\u103d\\u104c-\\u109f\\s]))(\\u1047)", "\u101b");
+    output_text = output_text.replaceAll("(\\u1047)(?=[\\u1000-\\u101c\\u101e-\\u102a\\u102c\\u102e-\\u103d\\u104c-\\u109f\\s])", "\u101b");
+    output_text = output_text.replaceAll("((?:\\u1031)?)([\\u1000-\\u1021])((?:\\u1039[\\u1000-\\u1021])?)((?:[\\u102d\\u102e\\u1032])?)([\\u1036\\u1037\\u1038]{0,2})([\\u103b-\\u103d]{0,3})((?:[\\u102f\\u1030])?)([\\u1036\\u1037\\u1038]{0,2})((?:[\\u102d\\u102e\\u1032])?)", "$2$3$6$1$4$9$7$5$8");
     output_text = output_text.replaceAll("\\u1036\\u102f", "\u102f\u1036");
     output_text = output_text.replaceAll("(\\u103a)(\\u1037)", "$2$1");
     return output_text;
@@ -246,16 +238,20 @@ public class Converter {
 
   public static int detector(String string) {
     //if (!string.matches("[\\u1000-\\u1097]")) {
-    //  return 0;}
-      String[] zawgyi = {
-          "ာ္", "်ာ", "[\\x20\\t\\r\\n\\f]" + "(ျ|ေ|[ၾ-ႄ])[က-အ]", "^(ျ|ေ|[ၾ-ႄ])[က-အ]",
-          "[က-အ]္[^က-အ]", "ဥ္", "္း", "[ါ-ူေ်း](ျ|[ၾ-ႄ])[က-အ]", "ံု", "[က-အ]္ေ", "ၤ",
-          "္" + "[\\x20\\t\\r\\n\\f]", "ာေ", "[ါ-ူ်း]ေ[က-အ]", "ေေ", "ုိ", "္$"
-      };
+
+    String isMyanmar = "[က-အ]+";
+    Pattern isMyanmarPattern = Pattern.compile(isMyanmar);
+    Matcher isMyanmarMatcher = isMyanmarPattern.matcher(string);
+    if (!isMyanmarMatcher.find()){
+      return 0;
+    }
+
       String[] uni =
           { "ှ", "ဿ", "ည်", "န်", "င်", "ေး", "ော", "်း", "ဵ", "[ၐ-ၙ]", "^([က-အ]ြ|[က-အ]ေ)" };
     for (String anUni : uni) {
-      if (string.contains(anUni)) {
+      Pattern pattern = Pattern.compile(anUni);
+      Matcher matcher = pattern.matcher(string);
+      if (matcher.find()) {
         return 1;
       }
     }
