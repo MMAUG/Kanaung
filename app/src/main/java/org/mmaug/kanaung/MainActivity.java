c2c63e8c24c942938ae72c28f7ebc7450115ff1e
Ye Lin Aung <me@yelinaung.com>
2015-04-09 23:46:59 +0630

    remove Analytics

--- app/src/main/java/org/mmaug/kanaung/MainActivity.java
+++ app/src/main/java/org/mmaug/kanaung/MainActivity.java
@@ -9,72 +9,60 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
-import com.google.android.gms.analytics.HitBuilders;
-import com.google.android.gms.analytics.Tracker;
 import org.gdgyangon.kanaung.R;
 
 public class MainActivity extends ActionBarActivity {
 
-	@Override
-    protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-    // Get tracker.
-    Tracker t = ((Kanaung)getApplication()).getTracker(
-        Kanaung.TrackerName.APP_TRACKER);
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.main);
+    Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+    setSupportActionBar(toolbar);
 
-    // Set screen name.
-    t.setScreenName("Main Activity");
+    Bundle bundle = getIntent().getExtras();
 
-    // Send a screen view.
-    t.send(new HitBuilders.ScreenViewBuilder().build());
+    if (getIntent().hasExtra("LAUNCH") && bundle.getString("LAUNCH").equals("YES")) {
+      startService(new Intent(MainActivity.this, FlyService.class));
+    }
 
-		Bundle bundle = getIntent().getExtras();
+    Button launch = (Button) findViewById(R.id.btnStart);
+    launch.setOnClickListener(new OnClickListener() {
 
-		if(getIntent().hasExtra("LAUNCH") && bundle.getString("LAUNCH").equals("YES")) {
-			startService(new Intent(MainActivity.this, FlyService.class));
-		}
+      @Override
+      public void onClick(View v) {
+        startService(new Intent(MainActivity.this, FlyService.class));
+      }
+    });
 
-		Button launch = (Button)findViewById(R.id.btnStart);
-		launch.setOnClickListener(new OnClickListener() {
+    Button stop = (Button) findViewById(R.id.btnStop);
+    stop.setOnClickListener(new OnClickListener() {
 
-			@Override
-			public void onClick(View v) {
-				startService(new Intent(MainActivity.this, FlyService.class));
-			}
-		});
+      @Override
+      public void onClick(View v) {
+        stopService(new Intent(MainActivity.this, FlyService.class));
+      }
+    });
+  }
 
-		Button stop = (Button)findViewById(R.id.btnStop);
-		stop.setOnClickListener(new OnClickListener() {
+  @Override
+  protected void onResume() {
+    Bundle bundle = getIntent().getExtras();
 
-			@Override
-			public void onClick(View v) {
-				stopService(new Intent(MainActivity.this, FlyService.class));
-			}
-		});
-		
-	}
-
-	@Override
-	protected void onResume() {
-		Bundle bundle = getIntent().getExtras();
-
-		if(getIntent().hasExtra("LAUNCH") && bundle.getString("LAUNCH").equals("YES")) {
-			startService(new Intent(MainActivity.this, FlyService.class));
-		}
-		super.onResume();
-	}
+    if (getIntent().hasExtra("LAUNCH") && bundle.getString("LAUNCH").equals("YES")) {
+      startService(new Intent(MainActivity.this, FlyService.class));
+    }
+    super.onResume();
+  }
 
   @Override public boolean onCreateOptionsMenu(Menu menu) {
-    getMenuInflater().inflate(R.menu.menu,menu);
+    getMenuInflater().inflate(R.menu.menu, menu);
     return super.onCreateOptionsMenu(menu);
   }
 
   @Override public boolean onOptionsItemSelected(MenuItem item) {
-    if (item.getItemId()==R.id.menu){
-      Intent i  = new Intent(this,AboutActivity.class);
+    if (item.getItemId() == R.id.menu) {
+      Intent i = new Intent(this, AboutActivity.class);
       startActivity(i);
     }
     return super.onOptionsItemSelected(item);
